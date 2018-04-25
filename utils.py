335d3c9ecf486369ebbdf252aa4f833ab73a0c40
bruno <bruno@IBJ-X4000.i2r.a-star.edu.sg>
2018-04-25 13:33:21 +0800

    2d works on 2 moon wiht 1 lbl per class

--- utils.py
+++ utils.py
@@ -23,3 +23,33 @@ def get_jacobian(y,x):
         grads = tf.stack([tf.gradients(yi, x)[0] for yi in tf.unstack(y, axis=1)], axis=2)
     return grads    
 
+def plot_boundaries():
+    x_min =-1.5; x_max = 2.5; y_min = -1; y_max = 1.5; h = 0.01;
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    grid = np.vstack([xx.ravel(),yy.ravel()]).T
+
+    zz = sess.run(logits_pl,{x_pl:grid})
+    softmax = lambda x: 1/(1+np.exp(-x))
+    zz=softmax(zz)
+    _xx, mm = sess.run([samp,tan_consistency])
+
+    n_test = len(pred)
+    fig = plt.figure(figsize=(15,5))
+    plt.subplot(131)
+    cnt = plt.contourf(xx, yy, zz.reshape(xx.shape),200,cmap=plt.cm.coolwarm, alpha=0.4)
+    # plt.contour(xx, yy, zz.reshape(xx.shape),1,map=plt.cm.binary, alpha=0.5,linewidths=2,linestyles='solid')
+    utils.scatter_2_class(testx[:n_test],pred,s=8)
+    utils.scatter_2_class(trainx,trainy, marker='D',s=100)
+    plt.xlim([-1.5,2.5]);plt.ylim([-1,1.5])
+
+    # This is the fix for the white lines between contour levels
+    for c in cnt.collections:
+        c.set_edgecolor("face")
+        c.set_linewidth(1e-9)
+
+    plt.subplot(132)
+    plt.scatter(_xx[:,0],_xx[:,1], s=50, alpha=1.,c=mm,cmap='Reds',norm=None,linewidths=0.1,edgecolors='b')
+
+    plt.subplot(133)
+    plt.plot([j*params_dnn['gamma'] for j in jls])
+    plt.plot(xls)
