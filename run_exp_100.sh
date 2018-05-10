bcd4e25ab98cb7d50419f211835cf2faaebbc2df
bruno <bruno@IBJ-X4000.i2r.a-star.edu.sg>
2018-05-10 19:32:13 +0800

    script decoupeld cnn

---
+++ run_exp_100.sh
@@ -0,0 +1,23 @@
+path=./log_decoupled_100lbl
+script=train_decoupled_cifar.py
+
+python $script --logdir $path/g5e-3_e10_seed10 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False
+
+python $script --logdir $path/vanilla_seed10 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --vanilla True
+
+
+python $script --logdir $path/g5e-3_e10_seed20 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --seed 20
+
+python $script --logdir $path/vanilla_seed20 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --vanilla True --seed 20
+
+
+python $script --logdir $path/g5e-3_e10_seed30 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --seed 30
+
+python $script --logdir $path/vanilla_seed30 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --vanilla True --seed 30
+
+
+python $script --logdir $path/g5e-3_e10_seed40 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --seed 40
+
+python $script --logdir $path/vanilla_seed40 --gamma 0.005 --epsilon 10. --eta 1. --data_dir /tmp/data/cifar-10-python --labeled 100 --epoch 800 --large False --vanilla True --seed 40
+
+
